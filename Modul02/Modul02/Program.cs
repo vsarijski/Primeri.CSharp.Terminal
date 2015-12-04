1b8f1e57fe29748554ae78c0fab348bb2e1184ea
Вълко Сарийски <vsarijski@gmail.com>
2015-12-04 10:19:57 +0200

    Модул 02, Видео 01 - Добаване на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+
+
+			//Дефинуране на променливи
+
+			//Събиране на числа
+
+			//Писане в конзола
+		}
+	}
+}
