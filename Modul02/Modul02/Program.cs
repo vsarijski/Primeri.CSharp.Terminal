d979c81ce08a80c3257302972d919aff6920791d
Вълко Сарийски <vsarijski@gmail.com>
2015-12-04 10:41:58 +0200

    Проемнливи от тип int

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -10,10 +10,25 @@ namespace Modul02
 
 
 			//Дефинуране на променливи
+			int a = 0, b = 0;
+			int sum = 0;
+
+
 
 			//Събиране на числа
 
+			a = 55;
+			b = 10;
+			sum = a + b;
+
+							
 			//Писане в конзола
+			Console.Write ( a);
+			Console.Write ( " + " );
+			Console.Write ( b );
+			Console.Write (" = ");
+			Console.WriteLine ( sum );
+
 		}
 	}
 }
